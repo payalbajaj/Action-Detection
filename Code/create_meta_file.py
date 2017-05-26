21be74ca92930a6522366407d3dde631842e4268
payalbajaj <pabajaj@stanford.edu>
2017-05-26 01:05:20 -0700

    meta file code updated

--- Code/create_meta_file.py
+++ Code/create_meta_file.py
@@ -6,8 +6,7 @@ import math
 import collections
 
 meta = {}
-action_id_mapping = {}
-act_iter = 0
+action_id_mapping = {'HighJump': 40, 'ThrowDiscus': 93, 'PoleVault': 68, 'VolleyballSpiking': 97, 'BasketballDunk': 9, 'CleanAndJerk': 21, 'Billiards': 12, 'JavelinThrow': 45, 'CricketShot': 24, 'FrisbeeCatch': 31, 'HammerThrow': 36, 'BaseballPitch': 7, 'TennisSwing': 92, 'CliffDiving': 22, 'CricketBowling': 23, 'Diving': 26, 'LongJump': 51, 'GolfSwing': 33, 'Shotput': 79, 'SoccerPenalty': 85}
 video_id_mapping = {}
 video_iter = 0
 video_duration = collections.defaultdict(lambda: 1.0)
@@ -18,8 +17,6 @@ def time_to_frame_num(i,t):
 
 for filename in os.listdir("./../annotation/"):
 	if("Ambiguous" not in filename):
-		action_id_mapping[filename.split("_")[0]] = act_iter
-		act_iter += 1
 		f = open(os.path.join("./../annotation/", filename))
 		for line in f:
 			video_name, _, start_time, end_time = line.replace("\n","").split(" ")
@@ -29,7 +26,10 @@ for filename in os.listdir("./../annotation/"):
 				meta[video_iter] = {}
 				meta[video_iter]['vidName'] = video_name
 				#meta[video_iter]['seq'] = [start_frame, end_frame]
-				meta[video_iter]['dets'] = [{}]*20
+				meta[video_iter]['dets'] = {}
+				for action in action_id_mapping:
+					meta[video_iter]['dets'][action_id_mapping[action]] = {}
+			# print filename.split("_")[0], action_id_mapping[filename.split("_")[0]], meta[video_iter]['dets']
 			if(len(meta[video_iter]['dets'][action_id_mapping[filename.split("_")[0]]]) == 0):
 				meta[video_iter]['dets'][action_id_mapping[filename.split("_")[0]]] = [[time_to_frame_num(video_iter, start_time), time_to_frame_num(video_iter, end_time)]]
 			else:
